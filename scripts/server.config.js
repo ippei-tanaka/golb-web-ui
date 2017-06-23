df3f1e4419c020ec79e73f4a83701bc75151ef44
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2017-06-23 17:44:29 -0400

    Removed comments

--- scripts/server.config.js
+++ scripts/server.config.js
@@ -2,12 +2,10 @@ export default {
     "webHost": "localhost",
     "webPort": 3003,
     "adminRoot": "/admin",
-//    "adminDocRoot": "./static-dev/admin-client",
     "adminApiHostname": "localhost",
     "adminApiPort": 3000,
     "adminApiBasename": "admin-api",
     "publicRoot": "/",
-//    "publicDocRoot": "./static-dev/public-client",
     "publicApiHostname": "localhost",
     "publicApiPort": 3000,
     "publicApiBasename": "public-api"
