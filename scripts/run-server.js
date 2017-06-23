5e933193070d44c0bd46e2bbfe55a91fa934a5d2
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2017-06-23 02:04:54 -0400

    Removed some comments and unused dependencies

--- scripts/run-server.js
+++ scripts/run-server.js
@@ -1,26 +1,7 @@
 import {start} from '../dev-tools/server';
 import config from './server.config';
-//import path from 'path';
-//import fs from 'fs';
-//import {ArgumentParser} from 'argparse';
 
 (async () => {
-
-    //const parser = new ArgumentParser();
-
-    //parser.addArgument(['-c', '--config'], {defaultValue: "config.json"});
-
-    //const args = parser.parseArgs();
-    //const cwd = process.cwd();
-    /*
-    const config = await new Promise(
-        (resolve, reject) => fs.readFile(
-            path.resolve(cwd, args.config),
-            "utf-8",
-            (err, data) => err ? reject(err) : resolve(JSON.parse(data))
-        )
-    );
-    */
 
     start(config);
 
